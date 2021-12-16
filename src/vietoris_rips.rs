2deeba7b2b71f21ec3ed3cf4861b70c3dc2bf831
Shing Tak Lam <shingtaklam1324@gmail.com>
2021-12-16 17:34:21 +0000

    fix

--- src/vietoris_rips.rs
+++ src/vietoris_rips.rs
@@ -6,11 +6,11 @@ use nalgebra::DMatrix;
 use super::simplex::Simplex;
 use super::simplicial_complex::SimplicialComplex;
 
-fn vr_simplex_step(adj: &DMatrix<bool>, simplices: &BTreeSet<Simplex<usize>>) -> BTreeSet<Simplex<usize>> {
+fn vr_simplex_step(adj: &DMatrix<bool>, simplices: &BTreeSet<Simplex<usize>>, dim: bool) -> BTreeSet<Simplex<usize>> {
     let mut new_simplices = BTreeSet::new();
     let d = simplices.iter().map(|s| s.dim()).max().unwrap();
     for simplex in simplices {
-        if simplex.dim() < d {
+        if dim && simplex.dim() < d {
             continue;
         }
         let mut with_new_vert = (0..adj.nrows())
@@ -24,10 +24,10 @@ fn vr_simplex_step(adj: &DMatrix<bool>, simplices: &BTreeSet<Simplex<usize>>) ->
     simplices.union(&new_simplices).cloned().collect()
 }
 
-fn vr_simplices(adj: &DMatrix<bool>, simplices: BTreeSet<Simplex<usize>>) -> BTreeSet<Simplex<usize>> {
+fn vr_simplices(adj: &DMatrix<bool>, simplices: BTreeSet<Simplex<usize>>, dim: bool) -> BTreeSet<Simplex<usize>> {
     let mut simplices = simplices;
     loop {
-        let new_simplices = vr_simplex_step(&adj, &simplices);
+        let new_simplices = vr_simplex_step(&adj, &simplices, dim);
         if new_simplices.len() == simplices.len() {
             break;
         }
@@ -39,7 +39,7 @@ fn vr_simplices(adj: &DMatrix<bool>, simplices: BTreeSet<Simplex<usize>>) -> BTr
 fn vr_simplicial_complex(adj: &DMatrix<bool>) -> SimplicialComplex<usize> {
     // Insert all 0-simplices
     let simplices = (0..adj.nrows()).map(|i| Simplex::from_iter([i])).collect();
-    SimplicialComplex::new(vr_simplices(adj, simplices))
+    SimplicialComplex::new(vr_simplices(adj, simplices, true))
 }
 
 /// Given a distance matrix, and epsilon, return the Vietoris-Rips complex.
@@ -54,5 +54,7 @@ pub fn vietoris_rips(dist: &DMatrix<f64>, epsilon: f64) -> SimplicialComplex<usi
 /// We can do this as any simplex in VR_delta must be a simplex in VR_epsilon.
 pub fn vietoris_rips_step(dist: &DMatrix<f64>, epsilon: f64, k: SimplicialComplex<usize>) -> SimplicialComplex<usize> {
     let adj = dist.map(|x| x <= epsilon);
-    SimplicialComplex::new(vr_simplices(&adj, k.simplices))
+    // When we increase epsilon, we can't only add max dimension simplices, we also need to add
+    // lower dimension simplices as well.
+    SimplicialComplex::new(vr_simplices(&adj, k.simplices, false))
 }
--- tests/vietoris_rips.rs
+++ tests/vietoris_rips.rs
@@ -24,3 +24,23 @@ fn two_points() {
     let k1step = vietoris_rips_step(&dist, 1., k1.clone());
     assert_eq!(k1, k1step);
 }
+
+#[test]
+fn four_points() {
+    use std::f64::consts::SQRT_2;
+    let dist = dmatrix!
+        [0., 1., SQRT_2, 1.;
+         1., 0., 1., SQRT_2;
+         SQRT_2, 1., 0., 1.;
+         1., SQRT_2, 1., 0.];
+    let k0 = vietoris_rips(&dist, 0.);
+    assert_eq!(k0, SimplicialComplex::from(vec![vec![0], vec![1], vec![2], vec![3]]));
+    let k1 = vietoris_rips(&dist, 1.);
+    assert_eq!(k1, SimplicialComplex::from(vec![vec![0], vec![1], vec![2], vec![3], 
+            vec![0, 1], vec![1, 2], vec![2, 3], vec![0, 3]]));
+    let k_sqrt2 = vietoris_rips(&dist, SQRT_2);
+    assert_eq!(k_sqrt2, SimplicialComplex::solid(3));
+    assert_eq!(k1, vietoris_rips_step(&dist, 1., k0.clone()));
+    assert_eq!(k_sqrt2, vietoris_rips_step(&dist, SQRT_2, k0));
+    assert_eq!(k_sqrt2, vietoris_rips_step(&dist, SQRT_2, k1));
+}
